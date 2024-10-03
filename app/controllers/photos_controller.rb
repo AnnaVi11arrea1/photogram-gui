166382dee1974ad3a5c4ae87e078411c8f3aa003
Anna Villarreal <154029176+AnnaVi11arrea1@users.noreply.github.com>
2024-10-03 16:14:30 +0000

    photo index page  started

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,12 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+    render(:templates => "photos/index")
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+    photo_id = Photo.where({ :id => the_id}).at(0)
+    render({ :templates => "photos/index"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get("/photos", :controller => "photos", :action => "index")
+  get("/photos/:path_id", :controller => "photos", :action => "show")
+
   get("/", :controller => "users", :action => "index")
   get("/users", :controller => "users", :action => "index")
   get("/users/:the_id", :controller => "users", :action => "show")
